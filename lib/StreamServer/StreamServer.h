7a53c77f7cfa5c78a21060da3f199f29383dd82c
Vitor Viqueira <vitorccsiqueira@gmail.com>
2023-11-23 01:43:58 -0300

    add parameters to set jpeg size and quality

--- lib/StreamServer/StreamServer.h
+++ lib/StreamServer/StreamServer.h
@@ -1,12 +1,13 @@
 #ifndef STREAMSERVER_H
 #define STREAMSERVER_H
 #include <Arduino.h>
-#include "esp_http_server.h"
+#include <esp_http_server.h>
+#include <sensor.h>
 
 class StreamServer
 {
 public:
-    void init();
+    void init(framesize_t frameSize, int jpegQuality);
     void startStream();
     static esp_err_t index_handler(httpd_req_t *req);
     static esp_err_t stream_handler(httpd_req_t *req);
